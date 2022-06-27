ebd3c7d5b2a8bda1e4dcaef9c81c1b2ffc911aa4
B Steele <steeleb@caryinstitute.org>
2022-06-27 17:23:09 -0600

    progress in reducing processing time; sketch workflow for automation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sentinel_acquisition
-automated gathering of sentinel 2 acquisition schedule for the EPSCoR lakes
+automated gathering of sentinel 2 a&b acquisition schedule for the EPSCoR lakes
+
+scripts by B. Steele (steeleb@caryinstitute.org)
+
+workflow: 
+- check to see if new files avail -- need to grab file name bits from scratch script and 
+- if new files, download and process (this is the bulk of scratch script) -- add source file to these
+- move old to archive, process and save file with date
--- SENTINEL_SCRATCH.R
+++ SENTINEL_SCRATCH.R
@@ -12,6 +12,23 @@ library(tidyverse)
 # point to directories
 lakedir = 'shapefiles/'
 
+# LAKE FILES PROCESSING ----
+me_lakes=read_sf(file.path(lakedir, 'NHDLakeShapefiles.gdb'), 'me_lakes')
+nh_lakes=read_sf(file.path(lakedir, 'NHDLakeShapefiles.gdb'), 'nh_lakes')
+ri_lakes=read_sf(file.path(lakedir, 'NHDLakeShapefiles.gdb'), 'ri_lakes')
+sc_lakes=read_sf(file.path(lakedir, 'NHDLakeShapefiles.gdb'), 'sc_lakes')
+
+epscor_lakes = rbind(me_lakes, nh_lakes) %>% 
+  rbind(., ri_lakes) %>% 
+  rbind(., sc_lakes)
+
+epscor_lakes = st_transform(epscor_lakes,crs = 'EPSG:4326')
+
+epscor_lakes_meta = st_drop_geometry(epscor_lakes) %>% 
+  select('OBJECTID', 'Permanent_', 'GNIS_Name')
+
+# SENTINEL DOWNLOAD AND EXTRACT ----
+
 #point to website
 sntl_ap_web = 'https://sentinel.esa.int/web/sentinel/missions/sentinel-2/acquisition-plans'
 
@@ -50,16 +67,22 @@ for(i in 1:length(list2a_files)){
   #deal with multiple kml layers
   for(j in 1:length(layers)){
       kml_file = read_sf(file.path(tmp, paste0('2a_', i, '.kml')),layer = layers[j])
+      #clip to extent
+      kml_clip = st_crop(kml_file, 
+                         st_bbox(epscor_lakes))
       if(j == 1){
-        kml_comp = kml_file
+        kml_comp = kml_clip
       } else {
-        kml_comp <- rbind(kml_comp, kml_file)
+        kml_comp <- rbind(kml_comp, kml_clip)
       }
   }
-
-  #grab extended data from file
+  
+  ids = unique(kml_comp$Name)
+  
+  xml_collate = NULL
+  
+  #grab extended data from file - this is a little laborious, but works. Someday if libkml is avail for Windows, we won't need this step.
   for(j in 1:length(layers)){
-    ext_data <- data.frame(matrix(ncol = 10))
     xml_file = read_xml(file.path(tmp, paste0('2a_', i, '.kml')),layer = layers[j])
     listnodes = xml_find_all(xml_file,".//d1:ExtendedData")
     for(k in 1:length(listnodes)){
@@ -67,100 +90,108 @@ for(i in 1:length(list2a_files)){
       xml_names <- as_tibble(unlist(xml_attrs(xml_children(listnodes[k]))))
       xml_names <- xml_names$value
       }
+      #get values; bind with names
       xml_values <- xml_text(xml_children(listnodes[k]))
       xml_extract <- as_tibble(cbind(xml_names, xml_values)) %>% 
         pivot_wider(names_from = xml_names, values_from = xml_values)
       if(k == 1) {
-        xml_collate <- xml_extract
-      } else {
-        xml_collate <- full_join(xml_collate, xml_extract)
+        xml_collate = merge(xml_collate, xml_extract)
+      }
+      if(xml_extract$ID %in% ids){
+      xml_collate = full_join(xml_extract, xml_collate)
       }
     }
   }
   
+  #get the intersections
+  sent_inter <- st_intersection(epscor_lakes, kml_comp)
+  sent_inter <- sent_inter %>% 
+    rename(ID = Name)
+  
+  #drop geo and add metadata
+  sent_inter_meta <- left_join(sent_inter, xml_collate) %>% 
+    st_drop_geometry() %>% 
+    mutate(acqusition_date = format(as.POSIXct(ObservationTimeStart), '%Y-%m-%d')) %>% 
+    select(ID, acqusition_date, OBJECTID) %>% 
+    left_join(., epscor_lakes_meta)
+  
+  #join
+  if(i == 1){
+    sent2a_dates <- sent_inter_meta
+  } else {
+    sent2a_dates <- full_join(sent2a_dates, sent_inter_meta)
+  }
+    
+}
+
+#create a composite sf layer and grab all the extended metadata in the kml file
+
+for(i in 1:length(list2b_files)){
+  download.file(url = paste0(dlpath, list2b_files[i]),
+                destfile = file.path(tmp, paste0('2b_', i, '.kml')))
+  layers <- ogrListLayers(file.path(tmp, paste0('2b_', i, '.kml')))
+  
+  #deal with multiple kml layers
+  for(j in 1:length(layers)){
+    kml_file = read_sf(file.path(tmp, paste0('2b_', i, '.kml')),layer = layers[j])
+    #clip to extent
+    kml_clip = st_crop(kml_file, 
+                       st_bbox(epscor_lakes))
+    if(j == 1){
+      kml_comp = kml_clip
+    } else {
+      kml_comp <- rbind(kml_comp, kml_clip)
+    }
+  }
+  
+  ids = unique(kml_comp$Name)
+  
+  xml_collate = NULL
+  
+  #grab extended data from file - this is a little laborious, but works. Someday if libkml is avail for Windows, we won't need this step.
+  for(j in 1:length(layers)){
+    xml_file = read_xml(file.path(tmp, paste0('2b_', i, '.kml')),layer = layers[j])
+    listnodes = xml_find_all(xml_file,".//d1:ExtendedData")
+    for(k in 1:length(listnodes)){
+      if(k == 1){
+        xml_names <- as_tibble(unlist(xml_attrs(xml_children(listnodes[k]))))
+        xml_names <- xml_names$value
+      }
+      #get values; bind with names
+      xml_values <- xml_text(xml_children(listnodes[k]))
+      xml_extract <- as_tibble(cbind(xml_names, xml_values)) %>% 
+        pivot_wider(names_from = xml_names, values_from = xml_values)
+      if(k == 1) {
+        xml_collate = merge(xml_collate, xml_extract)
+      }
+      if(xml_extract$ID %in% ids){
+        xml_collate = full_join(xml_extract, xml_collate)
+      }
+    }
+  }
+  
+  #get the intersections
+  sent_inter <- st_intersection(epscor_lakes, kml_comp)
+  sent_inter <- sent_inter %>% 
+    rename(ID = Name)
+  
+  #drop geo and add metadata
+  sent_inter_meta <- left_join(sent_inter, xml_collate) %>% 
+    st_drop_geometry() %>% 
+    mutate(acqusition_date = format(as.POSIXct(ObservationTimeStart), '%Y-%m-%d')) %>% 
+    select(ID, acqusition_date, OBJECTID) %>% 
+    left_join(., epscor_lakes_meta)
+  
+  #join
+  if(i == 1){
+    sent2b_dates <- sent_inter_meta
+  } else {
+    sent2b_dates <- full_join(sent2b_dates, sent_inter_meta)
+  }
   
 }
 
+#save last files names
 
 
-NEED TO GET EXTENDED DATA THAT HAS THE INFO!
-
-# folder_path <- "//Users//myname//Downloads//cb_2014_39_tract_500k"
-# 
-# original_kml_file_name <- "cb_2014_39_tract_500k.kml"
-# 
-# new_kml_file_name <- "kml_fix.kml"
-# 
-# #Find these attributes in the <th></th> rows by looking at the original KML in a text editor
-# 
-# attributes <- c(
-#   
-#   "STATEFP",
-#   
-#   "COUNTYFP",
-#   
-#   "TRACTCE",
-#   
-#   "AFFGEOID",
-#   
-#   "GEOID",
-#   
-#   "NAME",
-#   
-#   "LSAD",
-#   
-#   "ALAND",
-#   
-#   "AWATER"
-#   
-# )
-# 
-# #####################
-# 
-# setwd(folder_path)
-# 
-# txt <- readLines(original_kml_file_name)
-# 
-# txt.df <- data.frame("kml_txt"=txt)
-# 
-# txt.df$row_num <- as.numeric(rownames(txt.df))
-# 
-# attributes_tags <- unlist(lapply(attributes, function(x) paste0("<th>", x, "</th>")))
-# 
-# matches <- txt.df[txt.df$kml_txt %in% attributes_tags, ]
-# 
-# matches$attribute_value_rows <- matches$row_num + 1
-# 
-# #Drop column before merge
-# 
-# matches$row_num <- NULL
-# 
-# vals <- merge(x=txt.df, y=matches, by.x='row_num', by.y='attribute_value_rows')
-# 
-# vals$kml_txt.x <- gsub("<td>|</td>", "", vals$kml_txt.x)
-# 
-# vals$group_num <- unlist(lapply(1:nrow(vals[vals$kml_txt.y == paste0('<th>', attributes[1], '</th>'), ]),
-#                                 
-#                                 function(i) rep(i, length(attributes))
-#                                 
-# ))
-# 
-# val_list <- data.frame("dlm"=unique(ave(vals$kml_txt.x, vals$group_num, FUN = function(x)
-#   
-#   paste0('<name>', paste(x,collapse = "|"),'</name>')))
-#   
-# )
-# 
-# val_list$row_num <- as.numeric(rownames(val_list))
-# 
-# placemark_rows <- txt.df[substr(txt.df$kml_txt,1,6) == '<Place', ]
-# 
-# placemark_rows$name_row <- placemark_rows$row_num + 1
-# 
-# vals_with_target_row <- data.frame(cbind(placemark_rows$name_row, as.character(val_list$dlm)))
-# 
-# colnames(vals_with_target_row) <- c("name_row", "dlm")
-# 
-# txt.df.temp <- merge(x=txt.df, y=vals_with_target_row, by.x='row_num', by.y='name_row', all.x=T)
-# 
-# txt.df.temp$final <- ifelse(is.na(txt.df.temp$dlm) == T, as.character(txt.df.temp$kml_txt), as.character(txt.df.temp$dlm))
+unlink(tmp, recursive = T)
